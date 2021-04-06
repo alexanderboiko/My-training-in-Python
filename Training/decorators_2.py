d9f53435d2cbe6f52fe3f9754cd4a6928c1dc5f5
shyri <shyrik888888@gmail.com>
2021-04-06 21:03:59 +0300

    add decorators_2

---
+++ Training/decorators_2.py
@@ -0,0 +1,20 @@
+# Функция - полноправный обьект
+# Внутренняя функция может захватывать переменные из внешней
+
+
+
+def logger(func):
+    def wrapper(*args):
+        print(f'{func.__name__} started')
+        result = func(*args)
+        print(f'{func.__name__} finished')
+        return result
+    return wrapper
+
+@logger
+def summ(a, b): # в этот момент summ = wrapper
+    return a + b
+
+if __name__ == '__main__':
+
+    print(summ(2, 3))
